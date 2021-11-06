50bc1a49311e7a8f5a4eb11e3c95616e22aa5e46
Jyuri <jeon1310@gmail.com>
2021-11-06 12:57:38 +0900

    activity_details.xml 완성

---
+++ app/src/main/java/kr/hs/emirim/w2015/today_tea/DetailsActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.w2015.today_tea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
